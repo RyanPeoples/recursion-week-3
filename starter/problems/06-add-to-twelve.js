b2667c2381fbea1c027d7a5c546d81152d33449d
Ryan Peoples <peoplesjermaine@gmail.com>
2022-02-01 11:06:18 -0500

    06 done

--- starter/problems/06-add-to-twelve.js
+++ starter/problems/06-add-to-twelve.js
@@ -12,8 +12,18 @@ addToTwelve([1, 12, 4, 7, 6]); // false
 addToTwelve([1]); // false
 ***********************************************************************/
 
-// your code here
-
+let addToTwelve = arr => {
+  if(arr.length===1) return false;
+  if(arr[0]+arr[1]!==12){
+    arr.shift();
+    return addToTwelve(arr);
+  } else return true;
+}
+console.log(addToTwelve([1, 3, 4, 7, 5])); // true
+console.log(addToTwelve([1, 3, 4, 7, 6])); // false
+console.log(addToTwelve([1, 11, 4, 7, 6])); // true
+console.log(addToTwelve([1, 12, 4, 7, 6])); // false
+console.log(addToTwelve([1])); // false
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
   module.exports = addToTwelve;
