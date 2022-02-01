837cea58dcde93ed32607f4ae040ae9895aed3cb
Ryan Peoples <peoplesjermaine@gmail.com>
2022-02-01 11:37:40 -0500

    07 done

--- starter/problems/07-exponent.js
+++ starter/problems/07-exponent.js
@@ -1,12 +1,12 @@
 /***********************************************************************
-Write a recursive function called `exponent` that takes two integers, 
+Write a recursive function called `exponent` that takes two integers,
 `num` and `power`, and returns `num` raised to the `power`th power. Your
 function should work when `num` or `power` are positive OR negative.
 
 Exponents are used to represent a number being multiplied by itself a
 given number of times:
 
-4^3 = 4 x 4 x 4 = 64
+5^5 = 5 x 5 x 5 x 5 x 5 = 3125
 
 Negative exponents represent the same action, but in the denominator instead
 of the numerator:
@@ -20,10 +20,30 @@ exponent(2, -2); // 1/4 (or 0.25)
 exponent(5, 5); // 3125
 ***********************************************************************/
 
-function exponent(b, n) {
-  // your code here
+const exponent = (n, b) => {
+
+  if (b === 0) {
+    return 1;
+  } else if (b === 1) {
+    return n;
+  }
+
+  let counter = b;
+  let negCounter = b;
+
+  if (b < 1) {
+    negCounter++;
+    return 1 / n * exponent(n, negCounter)
+  }
+
+  if (b > 1) {
+    counter--;
+    return n * exponent(n, counter)
+  }
+
+  if (counter === 0 || negCounter === 0) return n;
 }
-  
+
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
   module.exports = exponent;
